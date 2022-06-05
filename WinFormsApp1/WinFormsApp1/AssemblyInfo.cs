285cee583c2723694b45e0b189c12940ef3ce1e2
nanine <nanine29@gmail.com>
2022-06-05 19:30:41 +1000

    increment version

--- WinFormsApp1/WinFormsApp1/AssemblyInfo.cs
+++ WinFormsApp1/WinFormsApp1/AssemblyInfo.cs
@@ -8,6 +8,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyFileVersion("1.0.3.0")]
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyInformationalVersion("1.0.3+Branch.main.Sha.f81f2573516c498b7e59d083984d9679c6d0ece8")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyInformationalVersion("1.0.4+Branch.main.Sha.64359044fb271e61c454b5f3af052f05bbbe73e6")]
